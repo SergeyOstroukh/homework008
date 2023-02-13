ce8e8ff1cdc14f28be9db198ba6cfff46d1042b1
Sergey <hardmass3ffect@gmail.com>
2023-02-13 10:58:38 +0300

    homework008

---
+++ part054/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrix(int rows, int colums, int min, int max)
+{
+    int[,] arrayMatrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            arrayMatrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return arrayMatrix;
+}
+
+void PrintArrayMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < array.GetLength(1)-1) Console.Write($"{array[i,j],4} ");
+            else Console.Write($"{array[i,j],4}");
+        }
+        Console.Write(" ]");
+        Console.WriteLine();
+
+    }
+
+}
+
+void SortedRowsMaxToMin(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                int temp;
+                if (array[i, j] < array[i,k]) continue;
+                temp = array[i, j];
+                array[i, j] = array[i,k];
+               array[i,k] = temp;
+            }
+        }
+    }
+}
+
+
+
+
+int[,] creat = CreateMatrix(3, 4, 1, 50);
+PrintArrayMatrix(creat);
+Console.WriteLine();
+SortedRowsMaxToMin(creat);
+PrintArrayMatrix(creat);
+
+
+
---
+++ part056/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrix(int rows, int colums, int min, int max)
+{
+    int[,] arrayMatrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            arrayMatrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return arrayMatrix;
+}
+
+void PrintArrayMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < array.GetLength(1) - 1) Console.Write($"{array[i, j],4} ");
+            else Console.Write($"{array[i, j],4}");
+        }
+        Console.Write(" ]");
+        Console.WriteLine();
+
+    }
+
+}
+
+int[] SumElementsInRows(int[,] array)                    
+{
+    int[] arrayMatr = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sumElements = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sumElements = sumElements + array[i, j];
+
+        }
+        arrayMatr[i] = sumElements;
+        sumElements = 0;
+    }
+    return arrayMatr;
+}
+
+int RowsIndexWithMaxSumNumbers(int[] array)
+{
+    int minNumber = 0;
+    int indexNumberMin = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        minNumber = array[0];
+        if (array[i] < minNumber)
+        {
+            minNumber = array[i];
+            indexNumberMin = i;
+        }
+    }
+    return indexNumberMin;
+}
+
+
+int[,] create = CreateMatrix(4, 3, 1, 50);
+PrintArrayMatrix(create);
+Console.WriteLine();
+int[] sumElements = SumElementsInRows(create);
+RowsIndexWithMaxSumNumbers(sumElements);
+Console.WriteLine();
+int rowsIndex = RowsIndexWithMaxSumNumbers(sumElements);
+Console.WriteLine($"{rowsIndex+1} строка имеет наименьшую сумму всех элементов");
